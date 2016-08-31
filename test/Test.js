caad64f42aedab5971196dfacd83d33b69892343
Dave Aitken <dave.aitken@gmail.com>
2016-08-31 22:58:42 +0100

    Back to quickcheck tests
    - Got them running with:
        pulp browserify --main Test.Main -I test --standalone runTests > test/test-main.js
    
    then opening the test/test.html file directly in a browser

--- test/Test.js
+++ test/Test.js
@@ -3,6 +3,11 @@
 
 // module Test.Main
 
+exports.inspect = function (msg) {
+    console.log('hi there');
+    console.log(msg);
+};
+
 exports.tagname = function (obj) {
-  return obj.tagName;
+    return obj.tagName;
 };
--- test/Test.purs
+++ test/Test.purs
@@ -1,176 +1,178 @@
 module Test.Main where
 
-import Prelude (bind)
+import Prelude
 
-import Data.Array (length)
-import Data.Maybe (Maybe(..), isNothing)
+import Data.Array
+import Data.Maybe
 
-import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
 
+import Data.DOM.Simple.Unsafe.Element(HTMLElement(..))
+import Data.DOM.Simple.Types
 import Data.DOM.Simple.Element
-import Data.DOM.Simple.Document   (body, title, setTitle)
-import Data.DOM.Simple.Window     (globalWindow, navigator, document)
-import Data.DOM.Simple.Navigator  (language, appVersion, appName)
+import Data.DOM.Simple.Document
+import Data.DOM.Simple.Window
+import Data.DOM.Simple.Encode
+import Data.DOM.Simple.Ajax
+import Data.DOM.Simple.Events
+import Data.DOM.Simple.Navigator
 import Data.DOM.Simple.NodeList as NL
 
-import Test.Spec                  (describe, it)
-import Test.Spec.Runner           (run)
-import Test.Spec.Assertions       (shouldEqual)
-import Test.Spec.Reporter.Console (consoleReporter)
+import Test.QuickCheck
+
+foreign import inspect :: forall a. a -> Unit
 
 foreign import tagname :: forall a. a -> String
 
-main = run [consoleReporter] do
-  describe "purescript-simple-dom" do
-    describe "document" do
-      it "Able to get the title of a document" do
-        doc <- liftEff (document globalWindow)
-        docTitle <- liftEff (title doc)
-        docTitle `shouldEqual` "testTitle"
-      it "Able to set the title of a document" do
-        docTitle <- liftEff do
-          doc <- document globalWindow
-          setTitle "modifiedTitle" doc
-          title doc
-        docTitle `shouldEqual` "modifiedTitle"
+checkTagName shouldBe element = do
+  let name = tagname element
+  quickCheck' 1 $ name == shouldBe
 
-    describe "body" do
-      it "Able to get the body element of a document" do
-        docBody <- liftEff do
-          doc <- document globalWindow
-          body doc
-        tagname docBody `shouldEqual` "BODY"
-      it "Able to append a child" do
-        liftEff do
-          doc <- document globalWindow
-          docBody <- body doc
-          Just testDiv <- getElementById "test1" doc
-          appendChild docBody testDiv
+checkContents shouldBe element = do
+  contents <- textContent (element :: HTMLElement)
+  quickCheck' 1 $ contents == shouldBe
 
-    describe "elements" do
-      it "Able to look up element by id" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        tagname testDiv `shouldEqual` "DIV"
-        content <- liftEff (textContent testDiv)
-        content `shouldEqual` "testContent1"
-      it "Able to look up element by class name" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (querySelector ".test2" doc)
-        tagname testDiv `shouldEqual` "DIV"
-        content <- liftEff (textContent testDiv)
-        content `shouldEqual` "testContent2"
-      it "Able to look up element by tag name" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (querySelector "test3" doc)
-        tagname testDiv `shouldEqual` "DIV"
-        content <- liftEff (textContent testDiv)
-        content `shouldEqual` "testContent3"
-      it "Able to count items in a nodelist" do
-        doc <- liftEff (document globalWindow)
-        n <- liftEff do
-          testDivs <- querySelectorAll "div" doc
-          NL.length testDivs
-        n `shouldEqual` 2
-      it "Able to retrieve an item from a nodelist" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff do
-          testDivs <- querySelectorAll "div" doc
-          NL.item 0 testDivs
-        tagname testDiv `shouldEqual` "DIV"
-      it "Able to convert a nodelist into an array" do
-        doc <- liftEff (document globalWindow)
-        testDivs <- liftEff (querySelectorAll "div" doc)
-        nl1 <- liftEff (NL.nodeListToArray  testDivs)
-        nl2 <- liftEff (NL.nodeListToArray' testDivs)
-        length nl1 `shouldEqual` length nl2
+checkValue shouldBe element = do
+  val <- value (element :: HTMLElement)
+  quickCheck' 1 $ val == shouldBe
 
-    describe "element modification" do
-      it "Able to modify input's value" do
-        doc <- liftEff (document globalWindow)
-        Just testInput <- liftEff (getElementById "input1" doc)
-        liftEff (setValue "foo bar baz" testInput)
-        val <- liftEff (value testInput)
-        val `shouldEqual` "foo bar baz"
-      it "Able to modify element's content" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        liftEff (setTextContent "Hello" testDiv)
-        content <- liftEff (textContent testDiv)
-        content `shouldEqual` "Hello"
-      it "Able to set an attribute on an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        testDivAttribute <- liftEff do
-          setAttribute "data-test" "hello" testDiv
-          getAttribute "data-test" testDiv
-        testDivAttribute `shouldEqual` "hello"
-      it "Able to remove an attribute from an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        testDivHasAttribute <- liftEff (hasAttribute "data-test" testDiv)
-        testDivHasAttribute `shouldEqual` true
-        liftEff (removeAttribute "data-test" testDiv)
-        testDivHasAttribute' <- liftEff (hasAttribute "data-test" testDiv)
-        testDivHasAttribute' `shouldEqual` false
-      it "Able to set a style attribute on an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        liftEff (setStyleAttr "color" "red" testDiv)
-        testDivStyleAttr <- liftEff (getStyleAttr "color" testDiv)
-        testDivStyleAttr `shouldEqual` "red"
+main = do
+  log "hi there son"
 
-    describe "navigator" do
-      it "Able to receive the appName from navigator" do
-        navigator <- liftEff (navigator globalWindow)
-        name <- liftEff (appName navigator)
-        name `shouldEqual` "Node.js jsDom"
-      it "Able to receive the appVersion from navigator" do
-        navigator <- liftEff (navigator globalWindow)
-        ver <- liftEff (appVersion navigator)
-        ver `shouldEqual` "Node.js jsDom"
-      it "Able to receive the language from navigator" do
-        navigator <- liftEff (navigator globalWindow)
-        lang <- liftEff (language navigator)
-        lang `shouldEqual` "en-US"
+  doc <- document globalWindow
 
-    -- Tests for offset* properties of an element. These behave strangely
-    -- with Zombie.js, so these tests just ensure that a value is returned.
-    describe "offset" do
-      it "Able to get the offsetParent of an element" do
-        -- HTMLElement.offsetParent is "undefined" in Zombie.js
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        noParent <- liftEff (offsetParent testDiv)
-        isNothing noParent `shouldEqual` true
-      it "Able to get the offsetHeight of an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        heightVal <- liftEff (offsetHeight testDiv)
-        heightVal `shouldEqual` 0
-      it "Able to get the offsetWidth of an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        widthVal <- liftEff (offsetWidth testDiv)
-        widthVal `shouldEqual` 0
-      it "Able to get the offsetTop of an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        topVal <- liftEff (offsetTop testDiv)
-        topVal `shouldEqual` 0
-      it "Able to get the offsetLeft of an element" do
-        doc <- liftEff (document globalWindow)
-        Just testDiv <- liftEff (getElementById "test1" doc)
-        leftVal <- liftEff (offsetLeft testDiv)
-        leftVal `shouldEqual` 0
+  log "Able to get the title of a document"
 
-      -- Unavailable in Zombie
-      {-
-      it "Able to add a class" do
-        hasTestClass <- classContains "testClass" testDiv
-        hasTestClass `shouldEqual` false
+  docTitle <- title doc
+  quickCheck' 1 $ docTitle == "testTitle"
 
-        classAdd "testClass" testDiv
-        hasTestClass' <- classContains "testClass" testDiv
-        hasTestClass' `shouldEqual` true
-      -}
+  -- attrib <- "#testDiv1" `getAttribute` "hello"
+
+  log "Able to set the title of a document"
+
+  setTitle "modifiedTitle" doc
+  docTitle1 <- title doc
+
+  quickCheck' 1 $ docTitle1 == "modifiedTitle"
+
+
+  log "Able to get the body element of a document"
+
+  docBody <- body doc
+
+  checkTagName "BODY" docBody
+
+  log "Able to look up elements"
+
+  Just testDiv1 <- getElementById "test1" doc
+
+  checkTagName "DIV" testDiv1
+  checkContents "testContent1" testDiv1
+
+  Just testDiv2 <- querySelector ".test2" doc
+
+  checkTagName "DIV" testDiv2
+  checkContents "testContent2" testDiv2
+
+  Just testDiv3 <- querySelector "test3" doc
+
+  checkTagName "TEST3" testDiv3
+  checkContents "testContent3" testDiv3
+
+  testDivs <- querySelectorAll "div" doc
+
+  log "Able to count items in a nodelist"
+  NL.length testDivs >>= (\len -> quickCheck' 1 $ len == 2)
+
+  log "Able to retrieve an item from a nodelist"
+  Just testDiv4 <- NL.item 0 testDivs
+  checkTagName "DIV" testDiv4
+
+  log "Able to convert a nodelist into an array"
+  nl1 <- NL.nodeListToArray testDivs
+  nl2 <- NL.nodeListToArray' testDivs
+  quickCheck' 1 $ length nl1 == length nl2
+
+  log "Able to modify an elements contents"
+
+  setTextContent "Hello" testDiv1
+
+  checkContents "Hello" testDiv1
+
+
+  log "Able to modify a form element's value"
+
+  Just testInput1 <- getElementById "input1" doc
+
+  setValue "foo bar baz" testInput1
+
+  checkValue "foo bar baz" testInput1
+
+  log "Able to set an attribute on an element"
+
+  setAttribute "data-test" "hello" testDiv1
+  testDiv1Attribute <- getAttribute "data-test" testDiv1
+
+  quickCheck' 1 $ testDiv1Attribute == "hello"
+
+
+  log "Able to remove an attribute from an element"
+
+  testDiv1HasAttribute <- hasAttribute "data-test" testDiv1
+  quickCheck' 1 $ testDiv1HasAttribute == true
+  removeAttribute "data-test" testDiv1
+  testDiv1HasAttribute' <- hasAttribute "data-test" testDiv1
+  quickCheck' 1 $ testDiv1HasAttribute' == false
+
+  log "Able to set a style attribute on an element"
+
+  setStyleAttr "color" "red" testDiv1
+  testDiv1StyleAttr <- getStyleAttr "color" testDiv1
+
+  quickCheck' 1 $ testDiv1StyleAttr == "red"
+
+  navigator <- navigator globalWindow
+  log "Able to receive the appName from navigator"
+  appName navigator >>= (\name -> quickCheck' 1 $ name == "Node.js jsDom")
+  log "Able to receive the appVersion from navigator"
+  appVersion navigator >>= (\name -> quickCheck' 1 $ name == "Node.js jsDom")
+  log "Able to receive the language from navigator"
+  language navigator >>= (\name -> quickCheck' 1 $ name == "en-US")
+
+  log "Able to append a child"
+  appendChild docBody testDiv1
+
+  -- Tests for offset* properties of an element. These behave strangely
+  -- with Zombie.js, so these tests just ensure that a value is returned.
+
+  log "Able to get the offsetParent of an element"
+  -- HTMLElement.offsetParent is "undefined" in Zombie.js
+  noParent <- offsetParent testDiv1
+  quickCheck' 1 $ isNothing noParent
+
+  log "Able to get the offsetHeight of an element"
+  heightVal <- offsetHeight testDiv1
+  quickCheck' 1 $ heightVal == 0
+
+  log "Able to get the offsetWidth of an element"
+  widthVal <- offsetWidth testDiv1
+  quickCheck' 1 $ widthVal == 0
+
+  log "Able to get the offsetTop of an element"
+  topVal <- offsetTop testDiv1
+  quickCheck' 1 $ topVal == 0
+
+  log "Able to get the offsetLeft of an element"
+  leftVal <- offsetLeft testDiv1
+  quickCheck' 1 $ leftVal == 0
+
+  -- Unavailable in Zombie
+  -- log "Able to add a class"
+
+  --hasTestClass1 <- classContains "testClass" testDiv1
+  -- quickCheck' 1 $ hasTestClass1 == false
+
+  -- classAdd "testClass" testDiv1
+  -- hasTestClass2 <- classContains "testClass" testDiv1
+  -- quickCheck' 1 $ hasTestClass2 == true
